ab62905c0310b81a32c68eb3bd7f6f7cbee4a1af
BumpScosity <BumpScosity@outlook.com>
2023-05-22 05:41:05 +0300

    Comment

--- Kernel/input.c
+++ Kernel/input.c
@@ -1,24 +1,22 @@
 #include "input.h"
 #include "vga.h"
 
-void left(unsigned char key, int row, int col) {
-    if (key == 0x1C) { // check for the enter key scancode
+void arrow_keys(unsigned char key, int row, int col) {
+    switch(key) {
+        case 0x4B:
         row++;
         col = 0;
         move_cursor(row, col);
-    }
-}
+        break;
 
-void right(unsigned char key, int row, int col) {
-    if (key == 0x4D) { // check for right arrow key scancode
+        case 0x4D:
         if (col < VGA_WIDTH - 1) { // make sure there is a character to move forward to
             col++;
             move_cursor(row, col);
         }
-    }
-}
+        break;
 
-void arrow_keys(unsigned char key, int row, int col) {
-    left(key, row, col);
-    right(key, row, col);
+        default:
+        break;
+    }
 }
