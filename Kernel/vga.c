f7d4d8c5d58255ad1b0f89bb39435d77fd01baff
BumpScosity <BumpScosity@outlook.com>
2023-05-22 07:45:01 +0300

    Comment

--- Kernel/vga.c
+++ Kernel/vga.c
@@ -12,7 +12,7 @@ void move_cursor(int row, int col) {
     outb(0x3D5, (unsigned char)((position >> 8) & 0xFF));
 }
 
-void write_char_NM(char c, int color, int row, int col) { // NM(No Move) means it will not move the cursor
+void write_char_NM(char c, int color, int row, int col) {
     if (row >= 0 && row < VGA_HEIGHT && col >= 0 && col < VGA_WIDTH) {
         VGA_cell* vga_entry = &(vga[row][col]);
 
