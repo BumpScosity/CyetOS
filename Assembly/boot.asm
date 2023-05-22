0942a3e51b44e794c9a69d55e71d96fea2bbd7d6
BumpScosity <BumpScosity@outlook.com>
2023-05-22 07:51:47 +0300

    Comment

--- Assembly/boot.asm
+++ Assembly/boot.asm
@@ -1,11 +1,10 @@
-[org 0x7c00]                        
-KERNEL_LOCATION equ 0x1000
-                                    
+[org 0x7c00]
 
-mov [BOOT_DISK], dl                 
+KERNEL_LOCATION equ 0x8000  ; Kernel will be loaded at this memory location
 
-                                    
-xor ax, ax                          
+mov [BOOT_DISK], dl
+
+xor ax, ax
 mov es, ax
 mov ds, ax
 mov bp, 0x8000
@@ -15,18 +14,16 @@ mov bx, KERNEL_LOCATION
 mov dh, 2
 
 mov ah, 0x02
-mov al, dh 
+mov al, dh
 mov ch, 0x00
 mov dh, 0x00
 mov cl, 0x02
 mov dl, [BOOT_DISK]
 int 0x13                ; no error management, do your homework!
 
-                                    
 mov ah, 0x0
 mov al, 0x3
 int 0x10                ; text mode
-
 
 CODE_SEG equ GDT_code - GDT_start
 DATA_SEG equ GDT_data - GDT_start
@@ -39,7 +36,7 @@ mov cr0, eax
 jmp CODE_SEG:start_protected_mode
 
 jmp $
-                                    
+
 BOOT_DISK: db 0
 
 GDT_start:
@@ -69,22 +66,19 @@ GDT_descriptor:
     dw GDT_end - GDT_start - 1
     dd GDT_start
 
-
 [bits 32]
 start_protected_mode:
     mov ax, DATA_SEG
-	mov ds, ax
-	mov ss, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	
-	mov ebp, 0x90000		; 32 bit stack base pointer
-	mov esp, ebp
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    mov ebp, 0x90000     ; 32-bit stack base pointer
+    mov esp, ebp
 
     jmp KERNEL_LOCATION
 
-                                     
- 
-times 510-($-$$) db 0              
+times 510-($-$$) db 0
 dw 0xaa55
