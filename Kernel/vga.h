9f9360ed91eea986340cae52a2bf77da6c3b7024
BumpScosity <BumpScosity@outlook.com>
2023-05-22 05:17:33 +0300

    Comment

--- Kernel/vga.h
+++ Kernel/vga.h
@@ -6,4 +6,7 @@ typedef struct {
     int color;
 } VGA_cell;
 
+void move_cursor(int row, int col);
+void write_char_NM(char c, int color, int row, int col); // NM(No Move) means it will not move the cursor
+
 #endif // VGA_H
