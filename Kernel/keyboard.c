ca6f335ea817106720e605cdf40c0d8e15aa7cef
BumpScosity <BumpScosity@outlook.com>
2023-05-22 08:20:00 +0300

    Comment

--- Kernel/keyboard.c
+++ Kernel/keyboard.c
@@ -2,10 +2,39 @@
 #include "lib.h"
 #include "core.h"
 #include "vga.h"
+#include "input.h"
+
+void handle_key_press(unsigned char key, int row, int col, int color, bool shift, char ascii_map[59]) {
+    int ascii_map_size = sizeof(ascii_map) / sizeof(ascii_map[0]);
+    switch (key) {
+        case 0x0E: // Backspace
+            handle_backspace(row, col, color);
+            break;
+        case 0x4B: // Left arrow
+            handle_left_arrow(col);
+            break;
+        case 0x1C: // Enter
+            handle_enter(row, col);
+            break;
+        case 0x4D: // Right arrow
+            handle_right_arrow(col);
+            break;
+        case 0x48: // Up arrow
+            handle_up_arrow(row);
+            break;
+        case 0x50: // Down arrow
+            handle_down_arrow(row);
+            break;
+        default: // Other keys
+            handle_character(key, row, col, color, shift, ascii_map, ascii_map_size);
+            break;
+    }
+}
+
 
 void handle_keyboard() {
     unsigned char key;
-    char ascii_map[] = {
+    char ascii_map[59] = {
         0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0, 0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0
     };
     int ascii_map_size = sizeof(ascii_map) / sizeof(ascii_map[0]);
--- Kernel/keyboard.h
+++ Kernel/keyboard.h
@@ -1,6 +1,9 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include "core.h"
+
+void handle_key_press(unsigned char key, int row, int col, int color, bool shift, char ascii_map[59]);
 void handle_keyboard();
 
 #endif // KEYBOARD_H
