f45e08bcb12cfe31b191ef635aef14d780bd21d3
BumpScosity <BumpScosity@outlook.com>
2023-05-22 06:09:41 +0300

    Comment

--- Kernel/input.c
+++ Kernel/input.c
@@ -2,22 +2,22 @@
 #include "vga.h"
 
 void arrow_keys(unsigned char key, int row, int col) {
-    switch(key) {
-        case 0x4B:
-        if (col > 0) { // make sure there is a character to move back to
-            col++;
-            move_cursor(row, col);
-        }
-        break;
+    switch (key) {
+        case 0x4B: // Left arrow key
+            if (col > 0) { // make sure there is a character to move back to
+                col--;
+                move_cursor(row, col);
+            }
+            break;
 
-        case 0x4D:
-        if (col < VGA_WIDTH - 1) { // make sure there is a character to move forward to
-            col--;
-            move_cursor(row, col);
-        }
-        break;
+        case 0x4D: // Right arrow key
+            if (col < VGA_WIDTH - 1) { // make sure there is a character to move forward to
+                col++;
+                move_cursor(row, col);
+            }
+            break;
 
         default:
-        break;
+            break;
     }
 }
