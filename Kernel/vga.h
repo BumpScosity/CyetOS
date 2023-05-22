045c00bd22c1ee4dfc403e0fa43a0dfbe6de2958
BumpScosity <BumpScosity@outlook.com>
2023-05-22 11:29:45 +0300

    Comment

--- Kernel/vga.h
+++ Kernel/vga.h
@@ -4,6 +4,11 @@
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 
+typedef struct {
+    char character;
+    int color;
+} VGA_cell;
+
 void move_cursor(int row, int col);
 void write_char(char c, int color, int row, int col);
 
