a2969fa92c39f8be734990e89040f60f0fccd80b
BumpScosity <BumpScosity@outlook.com>
2023-05-22 06:13:17 +0300

    Comment

--- Kernel/input.c
+++ Kernel/input.c
@@ -6,14 +6,12 @@ void arrow_keys(unsigned char key, int row, int col) {
         case 0x4B: // Left arrow key
             col--;
             move_cursor(row, col);
-            break;
 
         case 0x4D: // Right arrow key
             if (col < VGA_WIDTH - 1) { // make sure there is a character to move forward to
                 col++;
                 move_cursor(row, col);
             }
-            break;
 
         default:
             break;
