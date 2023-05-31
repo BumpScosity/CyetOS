b718d60db19f2861c01200e5bd1baee03a755a41
BumpScosity <BumpScosity@outlook.com>
2023-05-31 12:47:25 +0300

    Commit

--- kernel/cmd.c
+++ kernel/cmd.c
@@ -9,21 +9,10 @@ void parse(cmds lines[VGA_HEIGHT], int *row, int *col) {
 
     (*row)++;
 
-    switch (*cmd) {
+    switch (lower(*cmd)) {
         case 'h':
             cmd++;
             write_char('1', 0x07, row, col, lines[*row]);
-            if (*cmd == 'e') {
-                cmd++;
-                write_char('2', 0x07, row, col, lines[*row]);
-                if (*cmd == 'l') {
-                    cmd++;
-                    write_char('3', 0x07, row, col, lines[*row]);
-                    if (*cmd == 'p') {
-                        write_string("Test", 0x07, row, col, lines[*row]);
-                    }
-                }
-            }
         default:
             write_string("> ", 0x07, row, col, lines[*row]);
             break;
